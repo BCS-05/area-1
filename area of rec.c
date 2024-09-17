06243a0a5c81f69d7972f2d5f4eb45e0cc8af98a
unknown <mjanjezclinto@gmail.com>
2024-09-17 10:37:34 +0300

    area

---
+++ area of rec.c
@@ -0,0 +1,14 @@
+//To find area of a rectangle
+#include <stdio.h>
+int main()
+{
+    int length=30;
+    int width=60;
+    int area;
+    area=length*width;
+    printf("The area of rectangle is %d",area);
+
+    return 0;
+}
+
+
